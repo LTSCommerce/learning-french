b79d8126e828c6558ef3174d290c52cbfa5d41ee
LTS <72404520+LTSCommerce@users.noreply.github.com>
2025-01-24 16:57:48 +0000

    compare strings ignore accents

--- vocab/code.js
+++ vocab/code.js
@@ -190,17 +190,24 @@
             }
         }
 
+        function compareStringsIgnoreAccents(str1, str2) {
+            // Normalize both strings by removing accents and converting to lowercase
+            let normalizedStr1 = str1.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+            let normalizedStr2 = str2.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+        
+            return normalizedStr1 === normalizedStr2;
+        }
+
         // ------------------------------------------------------------------------
         // Helper Functions - Slide Generation
         // ------------------------------------------------------------------------
 
         // Function to generate the HTML content for a slide
         function generateSlideContent(englishWord, isPlural, wordWithOne,wordWithDefiniteArticle, possessiveArticle,
-            frenchWord, guessableText) {
-            
+            frenchWord, guessableText) {            
 
             let cognate='';
-            if(wordWithOne.includes(englishWord)){
+            if(compareStringsIgnoreAccents(englishWord,frenchWord)){
                 cognate='<span class="flashy-text" title="this word is a cognate"><a href="https://docs.steinhardt.nyu.edu/pdfs/metrocenter/xr1/glossaries/ELA/GlossaryCognatesFrenchUpdated5-5-2014.pdf" target="_blank">cognate</a></span>';
             }
             const englishWordDisplay = `<span class="english-word hidden" data-revealed="false">${englishWord} ${cognate}</span>`;
